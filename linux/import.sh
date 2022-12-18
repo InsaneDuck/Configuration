1ce2bc16d611674ee51265af8208469cc7eb4034
Satya <39309626+InsaneDuck@users.noreply.github.com>
2022-12-18 20:20:46 +0530

    updated

--- linux/import.sh
+++ linux/import.sh
@@ -1,11 +1,27 @@
 echo "adding g14 repo"
-sh scripts/add-g14-repo.sh
+sudo sh -c "echo '[g14]' >> /etc/pacman.conf"
+sudo sh -c "echo 'SigLevel = DatabaseNever Optional TrustAll' >> /etc/pacman.conf"
+sudo sh -c "echo 'Server = https://arch.asus-linux.org' >> /etc/pacman.conf"
+sudo pacman -Syu
 
 echo "installing pacman packages"
 sudo xargs pacman -S --needed --noconfirm < configs/pacman-packages.txt
 
 echo "removing packages"
-sh scripts/remove-packages.sh
+sudo pacman -R --noconfirm gnome-software-packagekit-plugin
+sudo pacman -R --noconfirm gnome-software
+sudo pacman -R --noconfirm malcontent
+#sudo pacman -R --noconfirm gnome-console
+sudo pacman -Runs --noconfirm gnome-photos
+sudo pacman -Runs --noconfirm totem
+sudo pacman -Runs --noconfirm gnome-weather
+sudo pacman -Runs --noconfirm epiphany
+sudo pacman -Runs --noconfirm gnome-contacts
+sudo pacman -Runs --noconfirm eog
+sudo pacman -Runs --noconfirm gnome-maps
+sudo pacman -Runs --noconfirm gnome-music
+#reinstalling this to fix cache?
+sudo pacman -S --noconfirm archlinux-appstream-data
 
 echo "setting up rust"
 rustup default stable
@@ -39,7 +55,16 @@ echo "installing aur packages"
 xargs yay -S --needed --noconfirm < configs/yay-packages.txt
 
 echo "enabling installed services"
-sh scripts/services.sh
+sudo systemctl enable avahi-daemon
+sudo systemctl enable bluetooth
+sudo systemctl enable cronie
+sudo systemctl enable cups
+sudo systemctl enable docker
+#sudo systemctl enable emby-server
+sudo systemctl enable libvirtd
+#sudo systemctl enable portmaster
+sudo systemctl enable power-profiles-daemon
+sudo systemctl enable supergfxd
 
 echo "restoring extensions"
 busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Extensions InstallRemoteExtension s "extensions-sync@elhan.io"
@@ -49,7 +74,7 @@ dconf load /org/gnome/shell/extensions/extensions-sync/ < /home/satya/Github/con
 busctl --user call org.gnome.Shell /io/elhan/ExtensionsSync io.elhan.ExtensionsSync read
 
 echo "installing flatpak packages"
-sh scripts/flatpak.sh
+#sh scripts/flatpak.sh
 
 echo "setting up git"
 git config --global user.name "satya"
--- linux/scripts/add-g14-repo.sh
+++
@@ -1,4 +0,0 @@
-sudo sh -c "echo '[g14]' >> /etc/pacman.conf"
-sudo sh -c "echo 'SigLevel = DatabaseNever Optional TrustAll' >> /etc/pacman.conf"
-sudo sh -c "echo 'Server = https://arch.asus-linux.org' >> /etc/pacman.conf"
-sudo pacman -Syu
