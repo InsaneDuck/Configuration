5bf60e0db954cbf5159354fb57029918fc3733ae
Satya <39309626+InsaneDuck@users.noreply.github.com>
2023-02-23 01:04:04 +0000

    updated

--- linux/desktops/hyprland/setup.sh
+++ linux/desktops/hyprland/setup.sh
@@ -1,2 +1,13 @@
-pacman -S kitty
-yay -S hyprland-git
+#!/bin/bash
+#to exit script if any command fails
+set -e
+
+echo "Enabling Wayland"
+sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
+
+echo "Installing Packages"
+yay -S --noconfirm mutter-dynamic-buffering hyprland-nvidia-git
+
+echo "Copying Configs"
+cp hypr  ~/.config
+sudo cp hyprland-wrapped.desktop /usr/share/wayland-sessions/
