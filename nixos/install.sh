cd6e9021872db9ce6979762a310c44a344026fb7
Satya <39309626+InsaneDuck@users.noreply.github.com>
2023-08-26 20:05:40 +0100

    updated

--- nixos/install.sh
+++ nixos/install.sh
@@ -1,4 +1,5 @@
+sudo nix-channel --update
 sudo cp configuration.nix /etc/nixos/configuration.nix
 sudo cp packages.nix /etc/nixos/packages.nix
 
-sudo nixos-rebuild switch
+sudo nixos-rebuild switch --upgrade
