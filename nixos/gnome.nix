86ac4a94964eec632adf801710ef567184966b72
InsaneDuck <39309626+InsaneDuck@users.noreply.github.com>
2023-10-06 15:19:28 +0100

    updated

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -1,4 +1,18 @@
-{lib,config, pkgs, ...}:
-{
+{lib,config,pkgs,...}:
 
+{
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+         gnome-builder
+         gnome.dconf-editor
+         gnome.gnome-boxes
+         gnome.gnome-chess
+         gnome.gnome-tweaks
+         gnome.sushi
+         gnomeExtensions.clipboard-indicator
+         gnomeExtensions.dash-to-panel
+         gnomeExtensions.gsconnect
+         gnomeExtensions.unite
+      ];
 }
--- nixos/packages.nix
+++ nixos/packages.nix
@@ -18,19 +18,8 @@
          gcolor3
          git
          github-desktop
-         gnome-builder
-         gnome.dconf-editor
-         gnome.gnome-boxes
-         gnome.gnome-chess
-         gnome.gnome-tweaks
-         gnome.sushi
-         gnomeExtensions.clipboard-indicator
-         gnomeExtensions.dash-to-panel
-         gnomeExtensions.gsconnect
-         gnomeExtensions.unite
          gnupg
          godot_4
-         google-chrome
          gparted
          gradle
          gthumb
